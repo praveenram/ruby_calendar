35b7a46051b75031386e974d55ecd8469a4ba223
Praveenram <praveenram.ba@gmail.com>
2013-11-03 17:44:46 +0530

    Adding events table to the calendar database

--- main.rb
+++ main.rb
@@ -5,3 +5,12 @@ FileUtils.mkdir_p("db")
 FileUtils.touch("db/calendar.db")
 
 db = SQLite3::Database.new('db/calendar.db')
+
+db.execute <<-SQL
+  create table event (
+    id int primary key asc not null,
+    starttime real not null,
+    endtime real not null,
+    name text not null default "Untitled"
+  );
+SQL
