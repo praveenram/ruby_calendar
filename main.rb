b7ad120e1e735b317a8a91544f127ccc549323f9
Praveenram <praveenram.ba@gmail.com>
2013-11-06 23:59:54 +0530

    Rendering the menu over and over till exit is used, done with loop, blocks in ruby

--- main.rb
+++ main.rb
@@ -8,28 +8,35 @@ name = gets.chomp
 db = Database.new name
 
 menu = Menu.new
-menu.show name
-option = menu.process_input
 
-if option.to_i == 1
-	new_event = Event.new
-	p "== New Event =="
-	p "== Enter event details =="
-	print "Enter event name: "
-	event_name = gets.chomp
-	new_event.store(:name, event_name)
-	print "Enter event start time (mm/dd/yyyy hh:mm): "
-	start_time = gets.chomp
-	new_event.store(:start_time, start_time)
-	print "Enter event end time (mm/dd/yyyy hh:mm): "
-	end_time = gets.chomp
-	new_event.store(:end_time, end_time)
-	new_event.save
-end
+loop do
+	menu.show name
+	option = menu.process_input
 
-if option.to_i == 2
-	p "id, name, start_date, end_date"
-	Event.all.each do |event|
-		p event
+	if option.to_i == 1
+		new_event = Event.new
+		p "== New Event =="
+		p "== Enter event details =="
+		print "Enter event name: "
+		event_name = gets.chomp
+		new_event.store(:name, event_name)
+		print "Enter event start time (mm/dd/yyyy hh:mm): "
+		start_time = gets.chomp
+		new_event.store(:start_time, start_time)
+		print "Enter event end time (mm/dd/yyyy hh:mm): "
+		end_time = gets.chomp
+		new_event.store(:end_time, end_time)
+		new_event.save
+	end
+
+	if option.to_i == 2
+		p "id, name, start_date, end_date"
+		Event.all.each do |event|
+			p event
+		end
+	end
+
+	if option.to_i == 3
+		break
 	end
 end
