33d99bc9ff6fef0d71f47b3ec8faf72c4e75c481
Praveenram <praveenram.ba@gmail.com>
2013-11-06 23:12:28 +0530

    Fixing time validation with Integer class and using exception Handling

--- date.rb
+++ date.rb
@@ -6,8 +6,12 @@ module DateValidator
 		month = date.split("/")[0].to_i
 		day = date.split("/")[1].to_i
 		year = date.split("/")[2].to_i
-		hour = time.split(":")[0].to_i
-		mins = time.split(":")[1].to_i
-		day.between?(1,31) and month.between?(1,12) and year > 1900 and hour.between?(0,23) and mins.between?(0,59)
+		begin
+			hour = Integer(time.split(":")[0])
+			mins = Integer(time.split(":")[1])
+			day.between?(1,31) and month.between?(1,12) and year > 1900 and hour.between?(0,23) and mins.between?(0,59)
+		rescue
+			false
+		end
 	end
 end
